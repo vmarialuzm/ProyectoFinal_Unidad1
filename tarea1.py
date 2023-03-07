8378262b95548cba0f178b48f9943eba8edb9272
vmarialuzm <vmarialuzm@gmail.com>
2023-03-07 10:20:58 -0500

    añadí el archivo .gitignore

---
+++ tarea1.py
@@ -0,0 +1,9 @@
+class Libro:
+    def __init__(self,id, titulo, genero, ISBN, editorial, autor):
+        self.id=id
+        self.titulo=titulo
+        self.genero=genero
+        self.ISBN=ISBN
+        self.editorial=editorial
+        self.autor=autor
+
