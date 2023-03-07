bb5d5939af7954c53f74507e9834146e7824a887
vmarialuzm <vmarialuzm@gmail.com>
2023-03-07 12:16:39 -0500

    creé el método de la opción 2

--- tarea1.py
+++ tarea1.py
@@ -1,3 +1,6 @@
+import csv
+    
+
 class Libro:
     def __init__(self,id, titulo, genero, ISBN, editorial, autor):
         self.id=id
@@ -7,3 +10,26 @@ class Libro:
         self.editorial=editorial
         self.autor=autor
 
+    def opcion1(self):
+        pass
+
+    def opcion2(self):
+        print(f"Id: {self.id}")
+        print(f"Titulo: {self.titulo}")
+        print(f"Genero: {self.genero}")
+        print(f"ISBN: {self.ISBN}")
+        print(f"Editorial: {self.editorial}")
+        print(f"Autor: {self.autor}")
+
+    def opcion5(self):
+        input(f"Buscar libro por ISBN o por titulo?  ")
+    
+    
+with open("libros.csv","r") as file:
+    reader=csv.DictReader(file)
+
+    for read in reader:
+        libro=Libro(read["Id"],read["Titulo"],read["Genero"],read["ISBN"],read["Editorial"],read["Autor"])
+        libro.opcion2()
+        print("-"*100)  
+    
