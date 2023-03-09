6afa4b5af3eeb9c19d638060232c71eed89ed7bf
vmarialuzm <vmarialuzm@gmail.com>
2023-03-08 19:49:01 -0500

    añadí 4 funciones del archivo tarea2

--- tarea2.py
+++ tarea2.py
@@ -12,6 +12,69 @@ def listar_generacion(num_generacion):
         url_imagen_pokemon= """
     print(name_pokemon)
 
-def listar_forma()
 
-listar_generacion("1")
+url="https://pokeapi.co/api/v2/pokemon-shape/"
+
+
+def listar_forma(num_forma):
+    response=requests.get(url+num_forma)
+    
+    if response.status_code==200:
+        response_json=response.json()
+        name_pokemon=[ nombre["name"] for nombre in response_json["pokemon_species"]]
+        """ habilidad_pokemon=
+        url_imagen_pokemon= """
+    print(name_pokemon)
+
+url3="https://pokeapi.co/api/v2/ability/"
+
+def listar_habilidad(num_habilidad):
+    response=requests.get(url3+num_habilidad)
+    
+    if response.status_code==200:
+        response_json=response.json()
+
+        name_pokemon=[ nombre["pokemon"]["name"] for nombre in response_json["pokemon"]]
+        url_pokemon=[ url["pokemon"]["url"] for url in response_json["pokemon"]]
+
+        contador=len(name_pokemon)+1
+        for url in url_pokemon:
+            contador-=1
+            response=requests.get(url)
+            if response.status_code==200:
+                response_json=response.json()
+
+                habilidad_pokemon=[habilidad["ability"]["name"] for habilidad in response_json["abilities"]]
+                url_imagen_pokemon=response_json["sprites"]["front_default"]
+        
+            print(f"{name_pokemon[len(name_pokemon)-contador]},{habilidad_pokemon},{url_imagen_pokemon}")
+           
+url5="https://pokeapi.co/api/v2/type/"
+
+def listar_tipo(num_tipo):
+    response=requests.get(url5+num_tipo)
+    
+    if response.status_code==200:
+        response_json=response.json()
+
+        name_pokemon=[ nombre["pokemon"]["name"] for nombre in response_json["pokemon"]]
+        url_pokemon=[ url["pokemon"]["url"] for url in response_json["pokemon"]]
+
+        contador=len(name_pokemon)+1
+        for url in url_pokemon:
+            contador-=1
+            response=requests.get(url)
+            if response.status_code==200:
+                response_json=response.json()
+
+                habilidad_pokemon=[habilidad["ability"]["name"] for habilidad in response_json["abilities"]]
+                url_imagen_pokemon=response_json["sprites"]["front_default"]
+        
+            print(f"{name_pokemon[len(name_pokemon)-contador]},{habilidad_pokemon},{url_imagen_pokemon}")
+           
+
+
+#listar_generacion("1")
+#listar_forma("1")
+#listar_habilidad("1")
+#listar_tipo("1")
