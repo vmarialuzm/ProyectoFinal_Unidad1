14c6a301253f870bfb26d21975b3abac36867f90
vmarialuzm <vmarialuzm@gmail.com>
2023-03-08 20:38:31 -0500

    creé una clase para elgir las opciones

--- tarea2.py
+++ tarea2.py
@@ -1,9 +1,19 @@
 import requests
 
-url="https://pokeapi.co/api/v2/generation/"
+url1="https://pokeapi.co/api/v2/generation/"
 
 def listar_generacion(num_generacion):
-    response=requests.get(url+num_generacion)
+    """ response=requests.get(url1)
+    response_json=response.json()
+    opciones_generacion=[generacion["name"] for generacion in response_json["results"]]
+
+    for contador,opcion in enumerate(opciones_generacion,start=1):
+        print(f"Opcion {contador} -> {opcion}")
+        
+    num_generacion=input(f"Ingrese el número de opción a elegir: ") """
+
+
+    response=requests.get(url1+num_generacion)
     
     if response.status_code==200:
         response_json=response.json()
@@ -12,12 +22,13 @@ def listar_generacion(num_generacion):
         url_imagen_pokemon= """
     print(name_pokemon)
 
+#-----------------------------------------------------------------------
 
-url="https://pokeapi.co/api/v2/pokemon-shape/"
+url2="https://pokeapi.co/api/v2/pokemon-shape/"
 
 
 def listar_forma(num_forma):
-    response=requests.get(url+num_forma)
+    response=requests.get(url2+num_forma)
     
     if response.status_code==200:
         response_json=response.json()
@@ -25,6 +36,8 @@ def listar_forma(num_forma):
         """ habilidad_pokemon=
         url_imagen_pokemon= """
     print(name_pokemon)
+
+#-----------------------------------------------------------------------
 
 url3="https://pokeapi.co/api/v2/ability/"
 
@@ -48,7 +61,24 @@ def listar_habilidad(num_habilidad):
                 url_imagen_pokemon=response_json["sprites"]["front_default"]
         
             print(f"{name_pokemon[len(name_pokemon)-contador]},{habilidad_pokemon},{url_imagen_pokemon}")
-           
+
+#-----------------------------------------------------------------------
+
+url4="https://pokeapi.co/api/v2/pokemon-habitat/"
+
+
+def listar_habitat(num_habitat):
+    response=requests.get(url4+num_habitat)
+    
+    if response.status_code==200:
+        response_json=response.json()
+        name_pokemon=[ nombre["name"] for nombre in response_json["pokemon_species"]]
+        """ habilidad_pokemon=
+        url_imagen_pokemon= """
+    print(name_pokemon)
+
+#-----------------------------------------------------------------------
+          
 url5="https://pokeapi.co/api/v2/type/"
 
 def listar_tipo(num_tipo):
@@ -77,4 +107,22 @@ def listar_tipo(num_tipo):
 #listar_generacion("1")
 #listar_forma("1")
 #listar_habilidad("1")
-#listar_tipo("1")
+#listar_tipo("5")
+
+def elegir_opciones():
+
+    response=requests.get(url4)
+    response_json=response.json()
+    opciones_generacion=[i["name"] for i in response_json["results"]]
+
+    for contador,opcion in enumerate(opciones_generacion,start=1):
+        print(f"Opcion {contador} -> {opcion}")
+        
+    opcion_elegida=input(f"Ingrese el número de opción a elegir: ")
+    #listar_generacion(opcion_elegida)
+    #listar_forma(opcion_elegida)
+    #listar_habilidad(opcion_elegida)
+    listar_habitat(opcion_elegida)
+    #listar_tipo(opcion_elegida)
+
+elegir_opciones()
