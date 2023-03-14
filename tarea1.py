e4282dc92aa09b7f25c69ee73c9b57981f82f990
vmarialuzm <vmarialuzm@gmail.com>
2023-03-13 19:05:40 -0500

    agregue el metodo 10

--- tarea1.py
+++ tarea1.py
@@ -50,65 +50,41 @@ class Biblioteca:
         isbn=input(f"ISBN: ")
         editorial=input(f"Editorial: ")
         autor=input(f"Autor: ")
+        #se creo un nuevo objeto
         libro=Libro(id,titulo,genero,isbn,editorial,autor)
+        #se agregó a la lista de objetos
         self.libros.append(libro)
         print("Se agregó el libro exitosamente.")
-
-        """ #abriendo el archivo modo append
-        with open("libros.csv","a",newline='') as libros_csv:
-            # Crear un objeto escritor de CSV
-            writer=csv.writer(libros_csv)
-
-            # Pedir al usuario que ingrese los datos del libro
-            id=input(f"Id: ")
-            titulo=input(f"Titulo: ")
-            genero=input(f"Genero: ")
-            isbn=input(f"ISBN: ")
-            editorial=input(f"Editorial: ")
-            autor=input(f"Autor: ")
-
-            #escribir datos en el archivo csv
-            writer.writerow([id,titulo,genero,isbn,editorial,autor])
-            print("Se agregó el libro exitosamente.") """
 
 #-----------------------------------------------------------------------
 
     def eliminar_libro(self):
-        # Abrir el archivo libros.csv en modo lectura y escritura
-        with open("libros.csv", "r+") as libros_csv:
-            # Leer todas las filas del archivo y almacenarlas en una lista
-            filas = libros_csv.readlines()
-            
-            # Eliminar la última fila de la lista
-            filas.pop()
-
-            # Volver al inicio del archivo y escribir todas las filas de la lista en el archivo, sobrescribiendo el archivo original
-            libros_csv.seek(0)
-            libros_csv.writelines(filas)
-            libros_csv.truncate()
-            
-            print("El último libro ha sido eliminado exitosamente.")
+        print(f"¿Qué libro desea eliminar?")
+        self.listar_libros()
+        indice_libro=int(input("Ingrese el número de libro que desea eliminar: "))-1
+        self.libros.pop(indice_libro)
+        print("Se eliminó el libro exitosamente")
 
 #-----------------------------------------------------------------------
 
-    def buscar_isbn_titulo(self,libros):
+    def buscar_isbn_titulo(self):
         headers=["Id","Titulo","Genero","ISBN","Editorial","Autor"]
         buscar_libro=input(f"Buscar libro por ISBN o por titulo?  ")
 
-        opciones_isbn=[libro.isbn for libro in libros]
-        opciones_titulo=[libro.titulo for libro in libros]
+        opciones_isbn=[libro.isbn for libro in self.libros]
+        opciones_titulo=[libro.titulo for libro in self.libros]
         if buscar_libro =="ISBN":
             print(f"Tienes las siguientes opciones: {opciones_isbn}")
             respuesta_ISBN=input(f"Cuál eliges? ")
             
             # Crear una lista temporal para almacenar los libros que coinciden con la búsqueda
-            resultados1 = [libro for libro in libros if libro.isbn == respuesta_ISBN]
+            resultados_isbn = [libro for libro in self.libros if libro.isbn == respuesta_ISBN]
 
-            if len(resultados1) == 0:
+            if len(resultados_isbn) == 0:
                 print("No se encontraron resultados.")
             else:
                 # Iterar sobre la lista de resultados e imprimir los detalles de cada libro
-                lista_opciones=[[libro.id,libro.titulo,libro.genero,libro.isbn,libro.editorial,libro.autor] for libro in resultados1]
+                lista_opciones=[[libro.id,libro.titulo,libro.genero,libro.isbn,libro.editorial,libro.autor] for libro in resultados_isbn]
                 print(tabulate(lista_opciones,headers=headers, tablefmt="grid"))
 
                    
@@ -117,28 +93,29 @@ class Biblioteca:
             print(f"Tienes las siguientes opciones: {opciones_titulo}")
             respuesta_titulo=input(f"Cuál eliges? ")
 
-            resultados2 = [libro for libro in libros if libro.titulo == respuesta_titulo]
+            resultados_titulo = [libro for libro in self.libros if libro.titulo == respuesta_titulo]
 
-            if len(resultados2) == 0:
+            if len(resultados_titulo) == 0:
                 print("No se encontraron resultados")
             else:
-                lista_opciones=[[libro.id,libro.titulo,libro.genero,libro.isbn,libro.editorial,libro.autor] for libro in resultados2]
+                lista_opciones=[[libro.id,libro.titulo,libro.genero,libro.isbn,libro.editorial,libro.autor] for libro in resultados_titulo]
                 print(tabulate(lista_opciones,headers=headers, tablefmt="grid"))
 
 #-----------------------------------------------------------------------
 
-    #metodo para ordenar alfabeticamente por titulo
-    def ordenar_libros(self,libros):
-        opciones_titulo=[libro.titulo for libro in libros]
+    def ordenar_libros(self):
         headers=["Id","Titulo","Genero","ISBN","Editorial","Autor"]
+        opciones_titulo=[libro.titulo for libro in self.libros]
+        
         lista_desordenada=opciones_titulo
         lista_ordenada=sorted(lista_desordenada)
+
         lista_objetos=[]
+
         #recorremos la lista ordenada
         for titulo in lista_ordenada:
             #recorremos la lista de objetos
-            for libro in libros:
-                #aplicamos el metodo opcion2() segun el orden de la lista ordenada
+            for libro in self.libros:
                 if titulo==libro.titulo:
                     lista_objetos.append(libro)
 
@@ -147,18 +124,19 @@ class Biblioteca:
         
 #-----------------------------------------------------------------------
 
-    def buscar_autor_editorial_genero(self,libros):
+    def buscar_autor_editorial_genero(self):
         headers=["Id","Titulo","Genero","ISBN","Editorial","Autor"]
         buscar_libro=input(f"Buscar libro por autor,editorial o genero?  ")
 
-        opciones_autor=[libro.autor for libro in libros]
-        opciones_editorial=[libro.editorial for libro in libros]
-        opciones_genero=[libro.genero for libro in libros]
+        opciones_autor=[libro.autor for libro in self.libros]
+        opciones_editorial=[libro.editorial for libro in self.libros]
+        opciones_genero=[libro.genero for libro in self.libros]
+
         if buscar_libro=="autor":
             print(f"Tienes las siguientes opciones: {opciones_autor}")
             respuesta_autor=input(f"Cuál eliges? ")
 
-            resultados_autor=[libro for libro in libros if libro.autor==respuesta_autor]        
+            resultados_autor=[libro for libro in self.libros if libro.autor==respuesta_autor]        
             print("-"*100) 
             if len(resultados_autor)==0:
                 print("No se encontraron resultados")
@@ -170,7 +148,7 @@ class Biblioteca:
             print(f"Tienes las siguientes opciones: {opciones_editorial}")
             respuesta_editorial=input(f"Cuál eliges? ")
 
-            resultados_editorial=[libro for libro in libros if libro.editorial==respuesta_editorial]        
+            resultados_editorial=[libro for libro in self.libros if libro.editorial==respuesta_editorial]        
             print("-"*100) 
             if len(resultados_editorial)==0:
                 print("No se encontraron resultados")
@@ -182,7 +160,7 @@ class Biblioteca:
             print(f"Tienes las siguientes opciones: {opciones_genero}")
             respuesta_genero=input(f"Cuál eliges? ")
 
-            resultados_genero=[libro for libro in libros if libro.genero==respuesta_genero]        
+            resultados_genero=[libro for libro in self.libros if libro.genero==respuesta_genero]        
             print("-"*100) 
             if len(resultados_genero)==0:
                 print("No se encontraron resultados")
@@ -192,13 +170,13 @@ class Biblioteca:
 
 #-----------------------------------------------------------------------
 
-    def buscar_num_autores(self,libros):
+    def buscar_num_autores(self):
         headers=["Id","Titulo","Genero","ISBN","Editorial","Autor"]
         num_autores=int(input("Ingresar número de autores: "))
 
         libros_encontrados=[]
         #recorrer la lista de objetos
-        for libro in libros:
+        for libro in self.libros:
             #si la suma de comas + 1 de cada autor coincide con lo ingresado, lo agregamos a la lista temporal
             if libro.autor.count(" , ")+1 == num_autores:
                 libros_encontrados.append(libro)
@@ -208,30 +186,35 @@ class Biblioteca:
 
 #-----------------------------------------------------------------------
                    
-    def editar_libros(self,libros):
-        opciones_id=[libro.id for libro in libros]
-        edita_libro=int(input(f"Qué libro desea editar? {opciones_id} : "))
+    def editar_libros(self):
+        print(f"¿Qué libro desea editar?")
+        #listamos todos los libros
+        self.listar_libros()
+        indice_libro=int(input("Ingrese el número de libro que desea editar: "))-1
+        id=input(f"Id: ")
+        titulo=input(f"Titulo: ")
+        genero=input(f"Genero: ")
+        isbn=input(f"ISBN: ")
+        editorial=input(f"Editorial: ")
+        autor=input(f"Autor: ")
+        #creamos un objeto
+        libro=Libro(id,titulo,genero,isbn,editorial,autor)
+        #reeemplamos el objeto con el indice elegido con los datos nuevos
+        self.libros[indice_libro]=libro
+        print("Se editó el libro exitosamente")
 
-        #abrir el archivo csv para lectura
-        with open("libros.csv","r") as libros_csv:
-            # leer los datos del archivo csv en una lista
-            data=list(csv.reader(libros_csv))
+#-----------------------------------------------------------------------
 
-            # modificar los siguientes datos
-            data[edita_libro][0]=input(f"Id: ")
-            data[edita_libro][1]=input(f"Titulo: ")
-            data[edita_libro][2]=input(f"Genero: ")
-            data[edita_libro][3]=input(f"ISBN: ")
-            data[edita_libro][4]=input(f"Editorial: ")
-            data[edita_libro][5]=input(f"Autor: ")
+    def guardar_libros(self):
+        with open("libros.csv","w", newline='') as f:
+            writer=csv.writer(f)
+            #escribir la cabecera
+            writer.writerow(["Id","Titulo","Genero","ISBN","Editorial","Autor"])
+            for libro in self.libros:
+                writer.writerow([libro.id,libro.titulo,libro.genero,libro.isbn,libro.editorial,libro.autor])
+            print("Se agregó el libro exitosamente.") 
 
-            #abrir el archivo csv para escritura
-            with open("libros.csv","w",newline="") as file:
-                #escribir los datos modificados ne el archivo csv
-                writer=csv.writer(file)
-                writer.writerows(data)
-            print("Se editó el libro exitosamente.")
-
+#-----------------------------------------------------------------------
 
 def menu():
     #creamos el objeto
@@ -248,7 +231,8 @@ def menu():
         print("7. Buscar libros por autor, editorial o género")
         print("8. Buscar libros por número de autores")
         print("9. Editar o actualizar datos de un libro")
-        print("10. Salir")
+        print("10. Guardar libros")
+        print("0. Salir")
 
         opcion_elegida=input("Ingrese el número de opción a elegir: ")
 
@@ -262,8 +246,29 @@ def menu():
         
         elif opcion_elegida=="3":
             biblioteca.agregar_libro()
+
+        elif opcion_elegida=="4":
+            biblioteca.eliminar_libro()
+
+        elif opcion_elegida=="5":
+            biblioteca.buscar_isbn_titulo()
+        
+        elif opcion_elegida=="6":
+            biblioteca.ordenar_libros()
+
+        elif opcion_elegida=="7":
+            biblioteca.buscar_autor_editorial_genero()
+        
+        elif opcion_elegida=="8":
+            biblioteca.buscar_num_autores()
+        
+        elif opcion_elegida=="9":
+            biblioteca.editar_libros()
         
         elif opcion_elegida=="10":
+            biblioteca.guardar_libros()
+        
+        elif opcion_elegida=="0":
             break
 
 
@@ -273,19 +278,6 @@ def menu():
 if __name__ == '__main__':
     menu()
 
-#libro.opcion2(libros)
-
-#libro.opcion5(libros)
-
-#libro.opcion6(libros)
-    #libro.opcion3()   
-    #libro.opcion4()
-#libro.opcion7(libros)
-    #libro.opcion8(libros)
-#libro.opcion9(libros)
-    
-    
-#agregar una funcion de menu
      
     
     
